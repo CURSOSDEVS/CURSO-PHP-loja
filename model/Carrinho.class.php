bf76e7c88202936d2eba4d82df4a35bbdc1d3242
CLAUDISNEI <claudisneibello@gmail.com>
2021-06-07 14:33:38 -0300

    Definido classe Config para armazenar todas as constantes da aplicação, iniciado a criação das classes Produto, Clientes e Carrinho. Todo controller obrigatoriamente deve ter um modelo mas não necessariamente uma view

---
+++ model/Carrinho.class.php
@@ -0,0 +1,13 @@
+<?php   
+
+/**Está classe fará por exemplo consulta no banco de dados
+ * para verificar se ha pedidos dentro do carrinho ou não
+ * fazendo os cálculos necessário * 
+ */
+
+class Carrinho
+{
+    
+}
+
+?>
---
+++ model/Clientes.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**Está classe fará funções como inserte
+ * select, delete e ter varias views, para 
+ * adicionar cliente ao pedido, para editar dados do cliente
+ * para inserir clientes, verificar algo do cliente 
+  */
+
+  class clientes
+  {
+      
+  }
+
+
+?>
